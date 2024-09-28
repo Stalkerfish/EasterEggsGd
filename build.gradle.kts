63a40b3c9a4b32ffa0fd739159bc00ec90395c70
Stalkerfish <silva.allison2002@gmail.com>
2024-09-28 19:59:25 -0300

    refactor: refine the implementation of GodotBridge using gradle tooling

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,94 +13,107 @@ repositories {
     mavenLocal()
     mavenCentral()
     google()
+    maven("https://repo.gradle.org/gradle/libs-releases/")
+
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation("org.jetbrains.skiko:skiko-awt-runtime-linux-x64:0.8.12")
     implementation("io.github.ismai117:kottie:2.0.0")
+    implementation("org.gradle:gradle-tooling-api:8.10.2")
+    implementation("org.slf4j:slf4j-simple:1.7.32") // Simple SLF4J binding for console logging
+}
+
+val godotVersion = "4.2.2"
+val godotKotlinVersion = "0.9.0"
+
+val godotOs = when {
+    OperatingSystem.current().isWindows -> "win64"
+    OperatingSystem.current().isMacOsX -> "macos"
+    else -> "x11"   // Linux or BSD
+}
+
+val godotExecutableName = when {
+    OperatingSystem.current().isWindows -> "godot.windows.editor.x86_64.exe"
+    OperatingSystem.current().isMacOsX -> "godot.macos.editor.x86_64"
+    else -> "godot.linuxbsd.editor.x86_64"
+}
+
+val godotFolderName = "godot-kotlin-jvm_editor_x11_$godotKotlinVersion-$godotVersion"
+val godotPath = "${System.getProperty("user.home")}/.godot-kotlin/$godotFolderName"
+val godotExecutablePath = "$godotPath/$godotExecutableName"
+
+tasks.register("downloadGodot") {
+    doLast {
+        val godotFile = File(godotExecutablePath)
+        if (!godotFile.exists()) {
+            val url = "https://github.com/utopia-rise/godot-kotlin-jvm/releases/download/$godotKotlinVersion-$godotVersion/godot-kotlin-jvm_editor_${godotOs}_$godotKotlinVersion-$godotVersion.zip"
+
+            println("Downloading Godot with Kotlin bindings...")
+            URL(url).openStream().use { input ->
+                ZipInputStream(input).use { zip ->
+                    var entry = zip.nextEntry
+                    while (entry != null) {
+                        val filePath = File(godotPath, entry.name)
+                        if (!entry.isDirectory) {
+                            filePath.parentFile.mkdirs()
+                            filePath.outputStream().use {   output ->
+                                zip.copyTo(output)
+                            }
+                        }
+                        entry = zip.nextEntry
+                    }
+                }
+            }
+            println("Godot with Kotlin bindings has been downloaded and extracted.")
+
+            if (!OperatingSystem.current().isWindows)
+                godotFile.setExecutable(true)
+            else
+                println("Godot with Kotlin bindings is already installed.")
+        }
+    }
+}
+
+tasks.register("launchGodot") {
+    dependsOn("downloadGodot")
+    doLast {
+        val godotFile = file(godotExecutablePath)
+        if (!godotFile.exists()) {
+            throw GradleException("Godot executable not found. Please run the downloadGodot task first.")
+        }
+
+        val scene = project.findProperty("scene") as String?
+        val command = mutableListOf(godotExecutablePath, "--path", projectDir.absolutePath)
+        scene?.let { command.add(it) }
+
+        exec {
+            commandLine(command)
+            workingDir = projectDir
+        }
+    }
+}
+
+tasks.register("runGodotScene") {
+    dependsOn("downloadGodot")
+    doLast {
+        val scene = project.findProject("scene") as? String
+        if (scene != null) {
+            exec {
+                workingDir = projectDir
+                commandLine(godotExecutablePath, "--path", projectDir.absolutePath, scene)
+            }
+        } else
+            throw GradleException("Please specify a scene using -Pscene=path/to/scene.tscn")
+
+    }
 }
 
 godot {
     registrationFileBaseDir.set(projectDir.resolve("scripts"))
     isRegistrationFileHierarchyEnabled.set(true)
 
-    val godotVersion = "4.2.2"
-    val godotKotlinVersion = "0.9.0"
-
-    val godotOs = when {
-        OperatingSystem.current().isWindows -> "win64"
-        OperatingSystem.current().isMacOsX -> "macos"
-        else -> "x11"   // Linux or BSD
-    }
-
-    val godotExecutableName = when {
-        OperatingSystem.current().isWindows -> "godot.windows.editor.x86_64.exe"
-        OperatingSystem.current().isMacOsX -> "godot.macos.editor.x86_64"
-        else -> "godot.linuxbsd.editor.x86_64"
-    }
-
-    val godotFolderName = "godot-kotlin-jvm_editor_x11_$godotKotlinVersion-$godotVersion"
-    val godotPath = "${System.getProperty("user.home")}/.godot-kotlin/$godotFolderName"
-    val godotExecutablePath = "$godotPath/$godotExecutableName"
-
-    tasks.register("downloadGodot") {
-        doLast {
-            val godotFile = File(godotExecutablePath)
-            if (!godotFile.exists()) {
-                val url = "https://github.com/utopia-rise/godot-kotlin-jvm/releases/download/$godotKotlinVersion-$godotVersion/godot-kotlin-jvm_editor_${godotOs}_$godotKotlinVersion-$godotVersion.zip"
-
-                println("Downloading Godot with Kotlin bindings...")
-                URL(url).openStream().use { input ->
-                    ZipInputStream(input).use { zip ->
-                        var entry = zip.nextEntry
-                        while (entry != null) {
-                            val filePath = File(godotPath, entry.name)
-                            if (!entry.isDirectory) {
-                                filePath.parentFile.mkdirs()
-                                filePath.outputStream().use {   output ->
-                                    zip.copyTo(output)
-                                }
-                            }
-                            entry = zip.nextEntry
-                        }
-                    }
-                }
-                println("Godot with Kotlin bindings has been downloaded and extracted.")
-
-                if (!OperatingSystem.current().isWindows)
-                    godotFile.setExecutable(true)
-                else
-                    println("Godot with Kotlin bindings is already installed.")
-            }
-        }
-    }
-
-    tasks.register("runGodot") {
-        dependsOn("downloadGodot")
-        doLast {
-            exec {
-                workingDir = projectDir
-                commandLine(godotExecutablePath, "--path", projectDir.absolutePath)
-            }
-        }
-    }
-
-    tasks.register("runGodotScene") {
-        dependsOn("downloadGodot")
-        doLast {
-            val scene = project.findProject("scene") as? String
-            if (scene != null) {
-                exec {
-                    workingDir = projectDir
-                    commandLine(godotExecutablePath, "--path", projectDir.absolutePath, scene)
-                }
-            } else
-                throw GradleException("Please specify a scene using -Pscene=path/to/scene.tscn")
-
-        }
-    }
-	
 	// To enable Android Export.
     //isAndroidExportEnabled.set(true) 
 	
