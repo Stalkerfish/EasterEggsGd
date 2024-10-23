f0f1d7f697c6201ce4854c0f929ac14c68ef4b1b
Stalkerfish <silva.allison2002@gmail.com>
2024-10-23 06:50:02 -0300

    feat(network): make use of WebSocket to trade items
    improve Inventory in HomeActivity

---
+++ src/main/kotlin/dexApp/app/src/main/java/com/alliwonka/dexapp/Inventories.kt
@@ -0,0 +1,25 @@
+package com.alliwonka.dexapp
+
+class Inventory {
+    private val items: MutableList<Item> = mutableListOf()
+
+    fun addItem(item: Item) {
+        items.add(item)
+    }
+
+    fun removeItem(item: Item) {
+        items.remove(item)
+    }
+
+    fun getItems(): MutableList<Item> {
+        return items
+    }
+}
+
+object HomeInventory {
+    val homeInventory = Inventory()
+}
+
+object EarthInventory {
+    val earthInventory = Inventory()
+}
